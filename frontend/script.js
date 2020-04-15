c97fde381ec3437e889f792cfb5d547816edbaf5
Dylan <dylanfairbanks@icloud.com>
2020-04-15 14:59:05 -0600

    refactored script.js

--- frontend/script.js
+++ frontend/script.js
@@ -78,17 +78,70 @@ function loadNext(){
 
 function generatePlaylist(){
     playList.forEach(vid => {
-        const container = document.createElement('div');
-        container.setAttribute('class', 'row playlistItem');
+        const container = makeContainer();
+        const div1 = setupThumbnail(vid);
+        const div2 = setupVideoTitle(vid);
 
-        //thumbnail
+        container.appendChild(div1);
+        container.appendChild(div2);
+        thumbs.push(container);
+        plSection.appendChild(container);
+    });
+
+    function setupThumbnail(vid)
+    {
+        const image = makeImage(vid);
+        const timeDisplay = setupTimeDisplay(vid);
+        const div1 = makeThumbnail(image, timeDisplay);
+        return div1;
+    }
+
+    function setupVideoTitle(vid)
+    {
+        const div2 = document.createElement('div');
+        div2.setAttribute('class', 'col-lg-6 pr-0');
+        const name = document.createElement('p');
+        div2.appendChild(name);
+        name.textContent = vid.metadata.title;
+        return div2;
+    }
+
+    function makeThumbnail(image, timeDisplay)
+    {
         const div1 = document.createElement('div');
         div1.setAttribute('class', 'col-lg-6 thumbnail text-right align-bottom pl-0');
-        const image = document.createElement('img');
-        const timeDisplay = document.createElement('p');
+        div1.appendChild(image);
+        div1.appendChild(timeDisplay);
+        return div1;
+    }
 
+    function setupTimeDisplay(vid)
+    {
+        const timeDisplay = document.createElement('p');
+        var duration = new Date(null);
+        duration.setSeconds(vid.metadata.duration);
+        var MHSTime = duration.toISOString().substr(11, 8);
+        var newDur = trimDuration(MHSTime);
+        timeDisplay.textContent = newDur;
+        return timeDisplay;
+    }
+
+    function makeImage(vid)
+    {
+        const image = document.createElement('img');
+        image.setAttribute('class', 'img-fluid');
+        image.setAttribute('style', 'cursor:pointer;');
+        image.setAttribute('src', vid.thumbnails[0].url);
+        return image;
+    }
+
+    function makeContainer()
+    {
+        const container = document.createElement('div');
+        container.setAttribute('class', 'row playlistItem');
         //image event listener
-        container.addEventListener('click', function(){
+        container.addEventListener('click', function ()
+        {
             const index = thumbs.indexOf(this);
             nextVideo = playList[index];
             videoSource.setAttribute('src', nextVideo.assets[1].url);
@@ -96,39 +149,12 @@ function generatePlaylist(){
             videoTitle.textContent = nextVideo.metadata.title;
             description.textContent = nextVideo.metadata.description;
             videoPlayer.play();
-            playList.splice(index,1);
-            thumbs.splice(index,1);
+            playList.splice(index, 1);
+            thumbs.splice(index, 1);
             this.remove();
-            
-        })
-
-
-        //get duration
-        var duration = new Date(null);
-        duration.setSeconds(vid.metadata.duration);
-        var MHSTime = duration.toISOString().substr(11, 8);
-        var newDur = trimDuration(MHSTime);
-
-
-        timeDisplay.textContent = newDur;
-        image.setAttribute('class', 'img-fluid');
-        image.setAttribute('style', 'cursor:pointer;')
-        div1.appendChild(image);
-        div1.appendChild(timeDisplay);
-        image.setAttribute('src', vid.thumbnails[0].url);
-
-        //Title
-        const div2 = document.createElement('div');
-        div2.setAttribute('class', 'col-lg-6 pr-0');
-        const name = document.createElement('p');
-        div2.appendChild(name);
-        name.textContent = vid.metadata.title;
-
-        container.appendChild(div1);
-        container.appendChild(div2);
-        thumbs.push(container);
-        plSection.appendChild(container);
-    });
+        });
+        return container;
+    }
 }
 
 function trimDuration(duration){
