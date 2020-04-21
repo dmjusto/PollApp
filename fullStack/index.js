300e907e617374c93c35f9513c705b3ffb2e360e
Dylan <dylanfairbanks@icloud.com>
2020-04-21 13:43:20 -0600

    setup navbar and landing page route

--- fullStack/index.js
+++ fullStack/index.js
@@ -3,10 +3,23 @@
  **************/
 const express = require('express');
 
+
 /*****************
  * CONFIGURATION
  ****************/
 const app = express();
+app.set('view engine', 'ejs');
+
+
+
+/****************
+ * ROUTES
+ ****************/
+app.get('/', function(req, res){
+    res.render('landing');
+})
+
+
 
 /***************
  * START SERVER
